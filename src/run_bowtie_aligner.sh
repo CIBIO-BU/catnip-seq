bb97ace37191809358ed3a6fb06f0ecb69ca26c2
Nuno Fonseca <nuno.fonseca@gmail.com>
2025-10-16 18:35:33 +0100

    Tuned bowtie parameters to ensure a sensitivity up to 11% divergence. Misc changes

--- src/run_bowtie_aligner.sh
+++ src/run_bowtie_aligner.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 
 # Get input args
 INPUT_FASTA=$1 #FASTA file for a cluster
@@ -6,15 +7,23 @@ INDEX_NAME=$2
 THREADS=$3
 ALIGN_NAME=$4
 
+BOWTIE_K=20
 # echo "Running bowtie indexer with the following parameters:"
 # echo "Input FASTA: $INPUT_FASTA"
 # echo "Index File: $INDEX_NAME"
 # echo "Threads: $THREADS"
 
+set -o pipefail -e
+
 bowtie2-build --quiet -f --threads "$THREADS" "$INPUT_FASTA" "$INDEX_NAME"
 
-bowtie2 --quiet -f -k 20 --threads "$THREADS" -x "$INDEX_NAME" -U "$INPUT_FASTA" \
-| samtools sort -o "$ALIGN_NAME".bam #TODO: check if -k 20 is appropriate!!!!
+bowtie2 --quiet -f  -L 10 -N 1 -k $BOWTIE_K --very-sensitive --threads "$THREADS" -x "$INDEX_NAME" -U "$INPUT_FASTA" \
+| samtools sort -o "$ALIGN_NAME".bam 
 
-rm -f "${INPUT_FASTA}"
+# The script should not delete the input file, this should be done by the calling script
+#rm -f "${INPUT_FASTA}"
+
+# clean up
 rm -f "${INDEX_NAME}".*.bt2
+
+exit 0
